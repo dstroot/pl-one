de941c756062cf8cdb7ac4e506126a8fbff803f6
Dan Stroot <dan.stroot@gmail.com>
2019-02-16 06:23:11 -0800

    ci/cd coverage

--- src/components/Search/index.test.js
+++ src/components/Search/index.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Search from '../Search';
+import React from "react";
+import renderer from "react-test-renderer";
+import Search from "../Search";
 
-describe('Search', () => {
-  it('it should render', () => {
+// simulate window resize
+const fireResize = width => {
+  window.innerWidth = width;
+  window.dispatchEvent(new Event("resize"));
+};
+
+describe("Search", () => {
+  fireResize(400);
+  it("it should render", () => {
+    const component = renderer.create(<Search />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  fireResize(800);
+  it("it should render", () => {
     const component = renderer.create(<Search />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
--- src/pages/News/index.test.js
+++ src/pages/News/index.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { create } from "react-test-renderer";
 import { mount } from "enzyme";
+
 import News from "../News";
 
 describe("News", () => {
@@ -26,7 +27,6 @@ describe("News", () => {
     const h1 = getMountedComponent()
       .find("h1")
       .first();
-
     expect(h1.text()).toContain("News!");
   });
 });
